c365901cfb83ec03101f928f345beed794e3e210
fabioferrari00 <fabioferrari.ff00@gmail.com>
2025-07-18 13:55:26 +0200

    [feat] add ajax's call and add it in html

--- js/script.js
+++ js/script.js
@@ -2,3 +2,10 @@ const emailList = document.getElementById('email-list');
 
 const email = `https://flynn.boolean.careers/exercises/api/random/mail`;
 
+//creo un ciclo per generare 10 email
+for (let i = 0; i < 10; i++) {
+  //effettuo la chiamata ajax
+  axios.get(email).then((resp) => {
+    emailList.innerHTML += `<li>${resp.data.response}</li>`
+  });
+};
